fd3a76d2588182cbc0c45fa6cd55e02b4326a278
Nir <nirose@gmail.com>
2020-01-18 12:08:01 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.debug("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaaaa");
+    }
+}
