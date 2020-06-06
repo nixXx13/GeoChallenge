67d31542a26f0589c5a9607470adc314e243b43b
Nir <nirose@gmail.com>
2020-06-06 16:53:45 +0300

    Constants file

---
+++ src/main/java/Util/Constants.java
@@ -0,0 +1,12 @@
+package Util;
+
+public class Constants {
+
+    // TODO - replace this with prop file
+
+    public final static int SERVER_PORT = 4567;
+    public static final String SCORES_SERVICE_URL = "";
+    public static final String QUESTION_PROVIDER_DOCKER_IP = "172.18.0.2";
+    public static final int QUESTION_PROVIDER_DOCKER_PORT = 500;
+
+}
